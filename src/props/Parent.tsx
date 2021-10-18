9ec143958446198fbd96b81fd40a822a9eb0ded6
paige <paige@paiges-Mac-mini.local>
2021-10-18 18:21:54 +0900

    react typescript basic

---
+++ src/props/Parent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ChildAsFC } from "./Child";
+
+const Parent = () => {
+  return (
+    <ChildAsFC
+      color="red"
+      onClick={() => {
+        console.log("Clicked!");
+      }}
+    ></ChildAsFC>
+  );
+};
+
+export default Parent;
---
+++ src/state/GuestList.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const GuestList = () => {
+  const [name, setName] = useState("");
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setName("");
+    setGuests([...guests, name]);
+  };
+
+  return (
+    <div>
+      <h3>Geust List</h3>
+      <ul>
+        {guests.map((guest) => (
+          <li>{guest}</li>
+        ))}
+      </ul>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Add Guest</button>
+    </div>
+  );
+};
+
+export default GuestList;
